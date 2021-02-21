12869b785634e7d0885872c4d74adb8424a0c79f
cxb1004 <17307565@qq.com>
2021-02-22 01:04:11 +0800

    调试通过

--- train.py
+++ train.py
@@ -7,7 +7,6 @@ import os
 import sys
 from snownlp import sentiment
 
-
 # 当前目录
 basePath = os.path.abspath(os.path.dirname(__file__))
 # 设置当前目录为执行运行目录
@@ -32,4 +31,19 @@ if not os.path.isfile(train_negative_file) or not os.path.isfile(train_positive_
 # 重新生成marshal文件
 removeFileIfExists(train_marshal_file)
 
+log.info('开始训练数据...')
+sentiment.train(neg_file=train_negative_file, pos_file=train_positive_file)
+log.info('数据训练完毕')
 
+log.info('保持训练结果文件...')
+sentiment.save(train_marshal_file, iszip=False)
+log.info('训练文件保存完毕')
+
+log.info('导入训练结果文件...')
+sentiment.load(train_marshal_file)
+log.info('导入训练结果文件完成:{}'.format(sentiment.data_path))
+
+
+text = '这真的是太舒服了，我太喜欢了'
+score = sentiment.classify(text)
+print('预测文本：{}\n预测结果：{}'.format(text,score))
