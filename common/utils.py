dee029bc1f0bd8f2ae84d26f72d67066332407cc
chengxb <chengxb@53kf.com>
2021-02-24 15:35:11 +0800

    新增两个func

--- common/utils.py
+++ common/utils.py
@@ -26,5 +26,27 @@ def replaceMutiSpace(str):
     str = re.sub(' +', ' ', str)
     return str
 
-# str = '123   456'
-# print(replaceMutiSpace(str))
+
+def remove53Emoji(str):
+    """
+    移除53自定义的表情符号
+    :param str:
+    :return:
+    """
+    patten = re.compile('{53b#\d*#}')
+    str = re.sub(patten, '', str)
+    return str
+
+
+def isFileExist(file):
+    """
+    判断文件是否存在
+    :param file:
+    :return:
+    """
+    if os.path.isfile(file):
+        return True
+    return False
+
+# txt = 'Hi~您好呀亲，我这边是官网小编，您是想拍婚纱照吗？{53b#32#}拍婚纱'
+# print(removeEmoji(txt))
